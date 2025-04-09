51412d5e24a895ce7208971fff0f58e2a04e4ce7
nakashima1231 <gabrielmnakashima2@gmail.com>
2025-04-08 21:33:07 -0300

    Update ex7.c

--- exercicios_01-04_02-04-main/ex7.c
+++ exercicios_01-04_02-04-main/ex7.c
@@ -3,14 +3,26 @@
 int main() {
     int n1, n2, n3, n4;
     
-    printf("linha 1: ");
-    scanf("%d", &n1);
-    printf("linha 2: ");
-    scanf("%d", &n2);
-    printf("linha 3: ");
-    scanf("%d", &n3);
-    printf("linha 4: ");
-    scanf("%d", &n4);
+    do {
+        printf("linha 1: ");
+        scanf("%d", &n1);
+    } while(n1 < 0 || n1 > 10);
+    
+    do {
+        printf("linha 2: ");
+        scanf("%d", &n2);
+    } while(n2 < 0 || n2 > 10);
+    
+    do {
+        printf("linha 3: ");
+        scanf("%d", &n3);
+    } while(n3 < 0 || n3 > 10);
+    
+    do {
+        printf("linha 4: ");
+        scanf("%d", &n4);
+    } while(n4 < 0 || n4 > 10);
+
     
     for(int i = 1; n1 >= i; n1--) {
         printf("*");
